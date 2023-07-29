c6aebe37cf0f3527ac11dee9cd93a92f45974a16
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2023-07-29 09:00:17 +0500

    wip

--- admin/src/utils/http.js
+++ admin/src/utils/http.js
@@ -3,8 +3,8 @@ export const imgBaseURL = "http://localhost:5000/";
 
 export const fetchApi = async (url, data) => {
   try {
-    const res = await fetch(`${baseURL}${url}`, data);
-    return res.ok ? res.json() : res;
+    const res = await fetch(`${baseURL}${url}`, data);  
+    return res.ok ? res.json() : res.json();
   } catch ({ message }) {
     return message;
   }
