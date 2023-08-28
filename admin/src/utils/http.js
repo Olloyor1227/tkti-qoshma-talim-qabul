b7df012a92dee5b764ffc61468c5b27a4b74922c
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2023-08-28 11:35:12 +0500

    fix: admin base url chnged

--- admin/src/utils/http.js
+++ admin/src/utils/http.js
@@ -1,8 +1,11 @@
-export const baseURL = "http://localhost:5000/api/";
-export const imgBaseURL = "http://localhost:5000/";
+// export const baseURL = "http://localhost:5000/api/";
+// export const imgBaseURL = "http://localhost:5000/";
 
 // export const baseURL = "https://tkti-qoshma-talim-qabul-47sy.vercel.app/api/";
 // export const imgBaseURL = "https://tkti-qoshma-talim-qabul-47sy.vercel.app/";
+
+export const baseURL = "https://api-qabul.tkti.uz/api/";
+export const imgBaseURL = "https://api-qabul.tkti.uz/";
 
 export const fetchApi = async (url, data) => {
   try {
